359a95e076fc33dcb63c866a5e8a5cae0bed3076
Eran Mani <eranmanim@gmail.com>
2021-04-26 00:09:47 +0300

    Added scenes - splash screen, main menu and level 1 Adjusted canvas resoultion and scaling in each scene

---
+++ Assets/Scripts/Level_Loader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_Loader : MonoBehaviour
+{
+    [SerializeField] float loadScreenTime;
+
+    int currentSceneIndex;
+
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(LoadStartScreen());
+        }  
+    }
+
+    IEnumerator LoadStartScreen()
+    {
+        yield return new WaitForSeconds(loadScreenTime);
+        SceneManager.LoadScene(currentSceneIndex+1);
+    }
+}
